5560e1c52ce62032aefbfc29432a1643816baeca
thetouchyhermes <ernestocioli2002@gmail.com>
2025-05-27 17:26:08 +0200

    OrderBook: replace TreeSet with ConcurrentSkipListSet, add complete() and incomplete()

--- src/main/java/it/unipi/cross/orderbook/OrderBook.java
+++ src/main/java/it/unipi/cross/orderbook/OrderBook.java
@@ -3,9 +3,9 @@ package it.unipi.cross.orderbook;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableSet;
-import java.util.TreeSet;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import it.unipi.cross.data.LimitOrder;
@@ -13,12 +13,12 @@ import it.unipi.cross.data.Order;
 
 public class OrderBook {
 
-   private final BlockingQueue<Order> newOrders = new LinkedBlockingQueue<>();
+   private final BlockingQueue<Command> newCommands = new LinkedBlockingQueue<>();
    private final Map<Integer, Order> currentOrders = new ConcurrentHashMap<>();
 
-   private final NavigableSet<LimitOrder> bidBook = new TreeSet<>(
+   private final NavigableSet<LimitOrder> bidBook = new ConcurrentSkipListSet<>(
          Comparator.comparingInt(LimitOrder::getPrice).reversed().thenComparingLong(LimitOrder::getTimestamp));
-   private final NavigableSet<LimitOrder> askBook = new TreeSet<>(
+   private final NavigableSet<LimitOrder> askBook = new ConcurrentSkipListSet<>(
          Comparator.comparingInt(LimitOrder::getPrice).thenComparingLong(LimitOrder::getTimestamp));
 
    public OrderBook() {
@@ -29,7 +29,7 @@ public class OrderBook {
 
    }
 
-   public boolean deleteOrder(int idOrder) {
+   public void deleteOrder(int idOrder) {
 
    }
 
@@ -39,6 +39,21 @@ public class OrderBook {
 
    public NavigableSet<LimitOrder> getAskBook() {
       return askBook;
+   }
+
+   public void complete(Order order) {
+      // remove from orderbook
+      // if market respond with orderId to user
+      // archive trade
+      // send notification of trade completion
+   }
+
+   public void incomplete(Order order) {
+      // remove from orderbook
+      // if market respond with -1 to user
+      // if stop respond with -1 to user
+      // archive trade
+      // send notification of trade completion
    }
 
 }
