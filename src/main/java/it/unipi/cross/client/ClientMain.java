1895686522b1bdbce8c75538e9c6ad5df1722660
thetouchyhermes <ernestocioli2002@gmail.com>
2025-06-06 10:21:00 +0200

    Add ClientMain and RequestFactory classes for TCP client functionality

---
+++ src/main/java/it/unipi/cross/client/ClientMain.java
@@ -0,0 +1,237 @@
+package it.unipi.cross.client;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import org.fusesource.jansi.AnsiConsole;
+
+import it.unipi.cross.config.ConfigReader;
+import it.unipi.cross.json.MessageResponse;
+import it.unipi.cross.json.OrderResponse;
+import it.unipi.cross.json.Request;
+import it.unipi.cross.json.Response;
+import it.unipi.cross.network.TcpClient;
+import it.unipi.cross.network.UdpListener;
+
+public class ClientMain {
+
+   private static TcpClient tcpClient;
+   private static UdpListener udpListener;
+   private static String username;
+   private static volatile boolean running = true;
+
+   public static void main(String[] args) {
+
+      // Enable Jansi
+      AnsiConsole.systemInstall();
+
+      // Start client app
+      Prompt.printStart();
+      
+      // Handler function for normal termination, exception and anomalous interruption
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+         running = false;
+
+         // close all connections
+         if (tcpClient != null && tcpClient.isAlive()) {
+            tcpClient.close();
+         }
+         if (udpListener != null) {
+            udpListener.shutdown();
+         }
+
+         Prompt.printEnd();
+
+         // Disable Jansi
+         AnsiConsole.systemUninstall();
+      }));
+
+      // load configuration
+      ConfigReader config = new ConfigReader();
+      try {
+         config.loadClient();
+      } catch (IOException e) {
+         System.err.println("[ClientMain] " + e.getMessage());
+         System.exit(1);
+      }
+
+      String udpAddress = config.getString("udp.address");
+      int udpPort = config.getInt("udp.port");
+      String tcpAddress = config.getString("tcp.address");
+      int tcpPort = config.getInt("tcp.port");
+      int tcpTimeout = config.getInt("tcp.timeout");
+
+      username = "";
+
+      // create and start UDP listener
+      udpListener = new UdpListener(udpAddress, udpPort);
+      Thread udpThread = new Thread(udpListener);
+      udpThread.start();
+
+      // Create and start TCP connection
+      tcpClient = new TcpClient(tcpAddress, tcpPort, tcpTimeout);
+
+      try{
+         tcpClient.connect();
+      } catch (IOException e) {
+         System.err.println("[ClientMain] Error during TCP connection");
+         System.exit(1);
+      }
+
+      try (Scanner scanner = new Scanner(System.in)) {
+
+         while (running) {
+            Prompt.newLine(username);
+            String line = "";
+            line = scanner.nextLine();
+            if (!running)
+               break;
+            if (line.isEmpty() || !line.contains("(") || !line.contains(")")) {
+               System.out.println("command format not valid");
+               continue;
+            }
+            String command = line.split("\\(")[0].trim();
+
+            if (command == null || command.isEmpty()) {
+               Prompt.printError("command is empty");
+               continue;
+            }
+            switch (command) {
+               case "register":
+                  if (!username.isEmpty()) {
+                     System.out.println("user currently logged in");
+                     continue;
+                  }
+                  break;
+               case "insertLimitOrder":
+               case "insertMarketOrder":
+               case "insertStopOrder":
+               case "cancelOrder":
+                  if (username.isEmpty()) {
+                     System.out.println("user not logged in");
+                     continue;
+                  }
+                  break;
+               case "updateCredentials":
+               case "login":
+               case "logout":
+               case "getPriceHistory":
+               case "help":
+               case "exit":
+                  break;
+               default:
+                  System.out.println("command not defined");
+                  continue;
+            }
+
+            Request request = RequestFactory.create(line);
+
+            if (request != null) {
+               String operation = request.getOperation();
+               Map<String, Object> values = request.getValues();
+               switch (operation) {
+                  case "notDefined":
+                     System.out.println("command not defined for this number of args");
+                     Prompt.printHelp(command);
+                     continue;
+                  case "invalidArgs":
+                     System.out.println("args not valid");
+                     Prompt.printHelp(command);
+                     continue;
+                  case "help":
+                     tcpClient.keepServerAlive();
+                     if (values == null || values.size() == 0) {
+                        Prompt.printHelp();
+                        continue;
+                     }
+                     String com = values.get("command").toString();
+                     if (com != null)
+                        Prompt.printHelp(com);
+                     continue;
+                  case "exit":
+                     System.exit(0);
+                  default:
+               }
+
+               System.out.println("ciao1");
+               Response response = tcpClient.sendRequest(request);
+               
+               if (response == null) {
+                  Prompt.printError("[ClientMain] Server closed the connection");
+                  running = false;
+                  continue;
+               }
+               // test
+               System.out.println(response.toString());
+
+               if (response instanceof MessageResponse) {
+                  MessageResponse messageResponse = (MessageResponse) response;
+
+                  int code = messageResponse.getResponse();
+                  String message = messageResponse.getErrorMessage();
+                  switch (code) {
+                     case 0:
+                        Prompt.printError("[ClientMain] Error on response received");
+                        System.exit(1);
+                     case 110:
+                        System.out.println(message);
+                        continue;
+                     case 100:
+                        switch (operation) {
+                           case "login":
+                              // to change after testing
+                              if (username.isEmpty()) {
+                                 if (values.get("username") != null) {
+                                    username = values.get("username").toString();
+                                 }
+                              } else {
+                                 Prompt.printError(" User was already logged before login");
+                                 System.exit(1);
+                              }
+                              break;
+                           case "logout":
+                              if (!username.isEmpty()) {
+                                 username = "";
+                                 System.exit(0);
+                              } else {
+                                 Prompt.printError("User was not logged before logout");
+                                 System.exit(1);
+                              }
+                        }
+                     default:
+                        System.out.print(response.toString());
+                        break;
+                  }
+               } else if (response instanceof OrderResponse) {
+                  OrderResponse orderResponse = (OrderResponse) response;
+
+                  int orderId = orderResponse.getOrderId();
+                  switch (orderId) {
+                     case 0:
+                        Prompt.printError("[ClientMain] Error on response received");
+                        System.exit(1);
+                     case -1:
+                        System.out.println("order request failed or was discarded:\n" + response.toString());
+                        break;
+                     default:
+                        System.out.println("order request placed correctly:\n" + response.toString());
+                        break;
+                  }
+               }
+            }
+         }
+      } catch (IOException e) {
+         Prompt.printError(e.getClass() + ": " + e.getMessage());
+         System.exit(1);
+      } catch (NoSuchElementException e) {
+         Prompt.printError("^C");
+         System.exit(2);
+      }
+
+      Prompt.printEnd();
+   }
+
+}
---
+++ src/main/java/it/unipi/cross/client/RequestFactory.java
@@ -0,0 +1,185 @@
+package it.unipi.cross.client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.unipi.cross.json.Request;
+
+public class RequestFactory {
+
+   public static Request create(String line) {
+
+      int parIdx1 = line.indexOf('(');
+      int parIdx2 = line.lastIndexOf(')');
+
+      String command = line.substring(0, parIdx1).trim();
+      String[] params = line.substring(parIdx1 + 1, parIdx2).split(",");
+
+      List<String> paramList = new ArrayList<>(Arrays.asList(params));
+      paramList.replaceAll(str -> str.trim());
+
+      System.out.println(command + " (" + String.join(", ", paramList) + ")");
+
+      Request request = new Request();
+      Map<String, Object> values = new LinkedHashMap<>();
+
+      request.setOperation(command);
+      request.setValues(values);
+
+      boolean emptyParam = false;
+      for (String str : paramList) {
+         if (str.isBlank()) {
+            emptyParam = true;
+            break;
+         }
+      }
+      if (emptyParam) {
+         switch (command) {
+            case "logout":
+            case "help":
+            case "exit":
+               break;
+            default:
+               request.setOperation("notDefined");
+               return request;
+         }
+      }
+
+      String type;
+      int size, price, orderId, month;
+
+      try {
+         switch (command) {
+            case "register":
+            case "login":
+               if (paramList.size() != 2) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               values.put("username", paramList.get(0));
+               values.put("password", paramList.get(1));
+               break;
+            case "updateCredentials":
+               if (paramList.size() != 3) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               values.put("username", paramList.get(0));
+               values.put("old_password", paramList.get(1));
+               values.put("new_password", paramList.get(2));
+               break;
+            case "insertLimitOrder":
+            case "insertStopOrder":
+               if (paramList.size() != 3) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               try {
+                  size = Integer.parseInt(paramList.get(1));
+                  price = Integer.parseInt(paramList.get(2));
+               } catch (NumberFormatException e) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+               type = paramList.get(0).toLowerCase();
+               if (!type.equals("ask") && !type.equals("bid") || size < 0 || price < 0) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+
+               values.put("type", type);
+               values.put("size", size);
+               values.put("price", price);
+               break;
+            case "insertMarketOrder":
+               if (paramList.size() != 2) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               try {
+                  size = Integer.parseInt(paramList.get(1));
+               } catch (NumberFormatException e) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+               type = paramList.get(0).toLowerCase();
+               if (!type.equals("ask") && !type.equals("bid") || size < 0) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+
+               values.put("type", type);
+               values.put("size", size);
+               break;
+            case "cancelOrder":
+               if (paramList.size() != 1) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               try {
+                  orderId = Integer.parseInt(paramList.get(0));
+               } catch (NumberFormatException e) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+               if (orderId <= 0) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+
+               values.put("orderId", orderId);
+               break;
+            case "getPriceHistory":
+               if (paramList.size() != 1) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+
+               try {
+                  month = Integer.parseInt(paramList.get(0));
+               } catch (NumberFormatException e) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+               if (month < 10000) {
+                  request.setOperation("invalidArgs");
+                  break;
+               }
+
+               values.put("month", paramList.get(0));
+               break;
+            case "help":
+               if (paramList.size() > 1) {
+                  request.setOperation("notDefined");
+                  break;
+               }
+               String com = paramList.get(0);
+               if (!com.isEmpty())
+                  values.put("command", com);
+               break;
+            case "logout":
+            case "exit":
+               if (paramList.size() != 1 || !paramList.get(0).isEmpty()) {
+                  request.setOperation("notDefined");
+               }
+               break;
+            default:
+               request.setOperation("notDefined");
+         }
+
+      } catch (IllegalArgumentException e) {
+         request.setOperation("notDefined");
+      }
+
+      return request;
+   }
+
+}
